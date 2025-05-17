46c32a0885b46b9fd8e02136fb6fff19cd4973f6
Abhishek <145379639+theabhishekbakshi@users.noreply.github.com>
2025-05-17 16:30:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let inputData = ""
 let page = 1;
 
 async function searchImages(){
-    inputData = inputEl.ariaValueMax;
+    inputData = inputEl.value;
     const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`
 
     const response = await fetch(url)
